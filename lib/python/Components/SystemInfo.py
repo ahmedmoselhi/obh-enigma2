3a63a37d1184c1c2b0f239676cd9140ed5a7dfe5
BarviX6 <barvix6@gmail.com>
2020-01-28 18:16:26 +0200

    Update SystemInfo.py

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -132,7 +132,7 @@ SystemInfo["VideoModes"] = getChipSetString() in ( # 2160p and 1080p capable har
 		["720p", "1080p", "2160p", "2160p30", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080p", "2160p", "2160p30", "1080i"} # widescreen modes
 	) or getChipSetString() in ( # 1080p capable hardware
-		'7241', '7356', '73565', '7358', '7362', '73625', '7424', '7425', '7552', '7111'
+		'7241', '7356', '73565', '7358', '7362', '73625', '7424', '7425', '7552', '7111', '7162'
 	) and (
 		["720p", "1080p", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080p", "1080i"} # widescreen modes
